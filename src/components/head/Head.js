ec6767a061244750b9ef2d4de2a5cf8adcb99165
Manav Joshi <manavmanishjoshi@gmail.com>
2024-05-11 22:41:16 +0530

    color change to red

--- src/components/head/Head.js
+++ src/components/head/Head.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import Summary from "../summary/Summary";
 function Head() {
   return (
-    <div className="hero min-h-screen bg-base-800">
+    <div className="hero min-h-screen bg-red-200">
       {/* hero-content  max-w-md */}
       <div className="hero-content text-center">
         <div className="max-w">
